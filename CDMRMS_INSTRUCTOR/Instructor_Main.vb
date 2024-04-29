e1aa31337071b1086511644ec6dd0f328f512800
John Irvin Geanga <johnirvingeanga@gmail.com>
2024-04-29 17:02:22 +0800

    UI Update

--- CDMRMS_INSTRUCTOR/Instructor_Main.vb
+++ CDMRMS_INSTRUCTOR/Instructor_Main.vb
@@ -46,7 +46,7 @@ Public Class Instructor_Main
     Private Sub Dropdown_Timer_Tick(sender As Object, e As EventArgs) Handles Dropdown_Timer.Tick
         If MenuCollapsed Then
 
-            Menu_Btn.Image = Image.FromFile("D:\Development Projects\Visual Basic\CDM Registrar Management System\CDMRMS_INSTRUCTOR\Assets\Main\Arrow_Down.png")
+            Menu_Btn.Image = Image.FromFile("D:\Development Projects\Visual Basic\CDM Registrar Management System\CDMRMS_INSTRUCTOR\Assets\Main\Arrow Down.png")
             Dropdown_Panel.Height += 10
             If Dropdown_Panel.Size = Dropdown_Panel.MaximumSize Then
 
@@ -58,7 +58,7 @@ Public Class Instructor_Main
 
             MyProfile_Panel.Hide()
             StudentGrade_Panel.Hide()
-            Menu_Btn.Image = Image.FromFile("D:\Development Projects\Visual Basic\CDM Registrar Management System\CDMRMS_INSTRUCTOR\Assets\Main\Arrow_Right.png")
+            Menu_Btn.Image = Image.FromFile("D:\Development Projects\Visual Basic\CDM Registrar Management System\CDMRMS_INSTRUCTOR\Assets\Main\Arrow Right.png")
             Dropdown_Panel.Height -= 10
             If Dropdown_Panel.Size = Dropdown_Panel.MinimumSize Then
 
